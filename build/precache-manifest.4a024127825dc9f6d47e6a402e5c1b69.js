a26dee8ca75c13c77a9fd9c2114c992ba3fd3c6a
SoniSSk <kuldeepsoni3099@gmail.com>
2021-12-03 12:28:25 +0530

    OfBussinessTask

---
+++ build/precache-manifest.4a024127825dc9f6d47e6a402e5c1b69.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9df947feb4b4ccb8d8c4",
+    "url": "/static/js/main.9df947fe.chunk.js"
+  },
+  {
+    "revision": "fb0a1d52dd5c14db8d21",
+    "url": "/static/css/1.5e306615.chunk.css"
+  },
+  {
+    "revision": "fb0a1d52dd5c14db8d21",
+    "url": "/static/js/1.fb0a1d52.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "484d238163d3a01c5285240e1ac0d665",
+    "url": "/index.html"
+  }
+];
